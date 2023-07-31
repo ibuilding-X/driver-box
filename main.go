8873b2b4fc2b73cf9813028e3a2fd176dc10729f
三刀 <zhengjunweimail@163.com>
2023-07-31 09:52:14 +0800

    将默认export调整成MQTT上报

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"github.com/ibuilding-x/driver-box/driverbox"
 	"github.com/ibuilding-x/driver-box/driverbox/export"
+	"os"
 )
 
 func main() {
-	driverbox.Start([]export.Export{&export.DefaultExport{}})
+	broker := os.Getenv("MQTT_BROKER")
+	clientId := os.Getenv("MQTT_CLIENT_ID")
+	username := os.Getenv("MQTT_USERNAME")
+	password := os.Getenv("MQTT_PASSWORD")
+	driverbox.Start([]export.Export{&export.MqttExport{
+		Broker:   broker,
+		ClientID: clientId,
+		Username: username,
+		Password: password,
+	}})
 }
