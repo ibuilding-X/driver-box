52b07a1edd3bcd4f9c220788d61b1dc8dad6ca34
jiyf18 <jiyf18@midea.com>
2024-05-16 19:24:02 +0800

    remove http server plugin

--- internal/plugins/httpserver/connector.go
+++ internal/plugins/httpserver/connector.go
@@ -1,91 +1,92 @@
 package httpserver
 
-import (
-	"context"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/ibuilding-x/driver-box/driverbox/plugin"
-	"github.com/ibuilding-x/driver-box/driverbox/plugin/callback"
-	"go.uber.org/zap"
-	"io"
-	"net/http"
-)
-
-type connectorConfig struct {
-	Host string `json:"host"`
-	Port uint16 `json:"port"`
-}
-
-type connector struct {
-	plugin  *Plugin
-	server  *http.Server
-	adapter plugin.ProtocolAdapter // 协议适配器
-}
-
-// Release 释放资源
-func (c *connector) Release() (err error) {
-	return c.server.Shutdown(context.Background())
-}
-
-// ProtocolAdapter 协议适配器
-func (p *connector) ProtocolAdapter() plugin.ProtocolAdapter {
-	return p.adapter
-}
-
-// Send 被动接收数据模式，无需实现
-func (c *connector) Send(raw interface{}) (err error) {
-	return nil
-}
-
-// startServer 启动服务
-func (c *connector) startServer(opts connectorConfig) {
-	// 启动服务
-	gin.SetMode(gin.ReleaseMode)
-	gin.DisableConsoleColor()
-	app := gin.Default()
-	// 通用路由
-	app.NoRoute(func(ctx *gin.Context) {
-		// 取 body
-		body, err := io.ReadAll(ctx.Request.Body)
-		if err != nil {
-			c.plugin.logger.Error("http request read body error", zap.Error(err))
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"code":    -1,
-				"message": err.Error(),
-			})
-			return
-		}
-		// 重组协议数据
-		data := protoData{
-			Path:   ctx.Request.URL.Path,
-			Method: ctx.Request.Method,
-			Body:   string(body),
-		}
-		// 调用回调函数
-		if _, err = callback.OnReceiveHandler(c, data.ToJSON()); err != nil {
-			c.plugin.logger.Error("http_server callback error", zap.Error(err))
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"code":    -1,
-				"message": err.Error(),
-			})
-			return
-		}
-		ctx.JSON(http.StatusOK, gin.H{
-			"code":    0,
-			"message": "ok",
-		})
-		return
-	})
-
-	addr := fmt.Sprintf("%s:%d", opts.Host, opts.Port)
-	c.server = &http.Server{
-		Addr:    addr,
-		Handler: app,
-	}
-
-	go func(addr string) {
-		if err := c.server.ListenAndServe(); err != nil {
-			c.plugin.logger.Error("start http server error", zap.Error(err))
-		}
-	}(addr)
-}
+//
+//import (
+//	"context"
+//	"fmt"
+//	"github.com/gin-gonic/gin"
+//	"github.com/ibuilding-x/driver-box/driverbox/plugin"
+//	"github.com/ibuilding-x/driver-box/driverbox/plugin/callback"
+//	"go.uber.org/zap"
+//	"io"
+//	"net/http"
+//)
+//
+//type connectorConfig struct {
+//	Host string `json:"host"`
+//	Port uint16 `json:"port"`
+//}
+//
+//type connector struct {
+//	plugin  *Plugin
+//	server  *http.Server
+//	adapter plugin.ProtocolAdapter // 协议适配器
+//}
+//
+//// Release 释放资源
+//func (c *connector) Release() (err error) {
+//	return c.server.Shutdown(context.Background())
+//}
+//
+//// ProtocolAdapter 协议适配器
+//func (p *connector) ProtocolAdapter() plugin.ProtocolAdapter {
+//	return p.adapter
+//}
+//
+//// Send 被动接收数据模式，无需实现
+//func (c *connector) Send(raw interface{}) (err error) {
+//	return nil
+//}
+//
+//// startServer 启动服务
+//func (c *connector) startServer(opts connectorConfig) {
+//	// 启动服务
+//	gin.SetMode(gin.ReleaseMode)
+//	gin.DisableConsoleColor()
+//	app := gin.Default()
+//	// 通用路由
+//	app.NoRoute(func(ctx *gin.Context) {
+//		// 取 body
+//		body, err := io.ReadAll(ctx.Request.Body)
+//		if err != nil {
+//			c.plugin.logger.Error("http request read body error", zap.Error(err))
+//			ctx.JSON(http.StatusInternalServerError, gin.H{
+//				"code":    -1,
+//				"message": err.Error(),
+//			})
+//			return
+//		}
+//		// 重组协议数据
+//		data := protoData{
+//			Path:   ctx.Request.URL.Path,
+//			Method: ctx.Request.Method,
+//			Body:   string(body),
+//		}
+//		// 调用回调函数
+//		if _, err = callback.OnReceiveHandler(c, data.ToJSON()); err != nil {
+//			c.plugin.logger.Error("http_server callback error", zap.Error(err))
+//			ctx.JSON(http.StatusInternalServerError, gin.H{
+//				"code":    -1,
+//				"message": err.Error(),
+//			})
+//			return
+//		}
+//		ctx.JSON(http.StatusOK, gin.H{
+//			"code":    0,
+//			"message": "ok",
+//		})
+//		return
+//	})
+//
+//	addr := fmt.Sprintf("%s:%d", opts.Host, opts.Port)
+//	c.server = &http.Server{
+//		Addr:    addr,
+//		Handler: app,
+//	}
+//
+//	go func(addr string) {
+//		if err := c.server.ListenAndServe(); err != nil {
+//			c.plugin.logger.Error("start http server error", zap.Error(err))
+//		}
+//	}(addr)
+//}
