7cde602b41a49803e3127482304f0c2b75853126
zhengjw22 <zhengjunweimail@163.com>
2024-05-17 15:47:44 +0800

    更新文档

---
+++ pages/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Driver-Box',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: '快速上手',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: '插件',
+					autogenerate: { directory: 'plugins' },
+				},
+			],
+		}),
+	],
+});
