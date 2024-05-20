b1d2831820b152afecbd47b6eae3cd953af7e512
xiaocai <1643013940@qq.com>
2024-05-20 10:54:09 +0800

    fix(http 路由注册): 屏蔽异常路由
    - 屏蔽 modbus 插件异常路由

--- internal/plugins/modbus/rest_api.go
+++ internal/plugins/modbus/rest_api.go
@@ -1,13 +1,7 @@
 package modbus
 
-import (
-	"github.com/ibuilding-x/driver-box/driverbox/restful"
-	"net/http"
-)
-
 func InitRestAPI() {
-	restful.HandleFunc("", func(request *http.Request) (any, error) {
-		return nil, nil
-	})
-
+	//restful.HandleFunc("", func(request *http.Request) (any, error) {
+	//	return nil, nil
+	//})
 }
