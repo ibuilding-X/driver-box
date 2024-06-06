65ea5b8bbb579ab4f09f2a651d7338fec7c9694b
jiyf18 <jiyf18@midea.com>
2024-06-06 19:52:12 +0800

    exports will start async

--- driverbox/driverbox.go
+++ driverbox/driverbox.go
@@ -44,9 +44,11 @@ func Start(exports []export.Export) error {
 	//第四步：启动Export
 	helper.Exports = exports
 	for _, item := range exports {
-		if err := item.Init(); err != nil {
-			helper.Logger.Error("init export error", zap.Error(err))
-		}
+		go func(e export.Export) {
+			if err := e.Init(); err != nil {
+				helper.Logger.Error("init export error", zap.Error(err))
+			}
+		}(item)
 	}
 
 	// 第五步：启动 REST 服务
