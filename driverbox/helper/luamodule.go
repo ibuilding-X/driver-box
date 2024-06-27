124bc5cbb656257de7f8eb4a512fa069d836a513
zhengjw22 <zhengjunweimail@163.com>
2024-06-27 18:42:02 +0800

    优化代码

--- driverbox/helper/luamodule.go
+++ driverbox/helper/luamodule.go
@@ -2,8 +2,6 @@ package helper
 
 import (
 	"fmt"
-	"github.com/ibuilding-x/driver-box/driverbox/plugin"
-	"github.com/ibuilding-x/driver-box/internal/export"
 	lua "github.com/yuin/gopher-lua"
 )
 
@@ -16,9 +14,9 @@ type LuaModule struct{}
 func (lm *LuaModule) Loader(L *lua.LState) int {
 	// 模块方法定义
 	mod := L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
-		"setCache":      lm.SetCache,
-		"getCache":      lm.GetCache,
-		"writeToMsgBus": lm.WriteToMsgBus,
+		"setCache": lm.SetCache,
+		"getCache": lm.GetCache,
+		//"writeToMsgBus": lm.WriteToMsgBus,
 	})
 	L.Push(mod)
 
@@ -54,34 +52,34 @@ func (lm *LuaModule) SetCache(L *lua.LState) int {
 // WriteToMsgBus 上报设备数据
 // deviceSn：设备SN，例如：sensor_1
 // points：点位数据，例如（此时以json格式进行说明，lua实际入参格式为 table 类型）：{"onOff":1, "voc": 23}
-func (lm *LuaModule) WriteToMsgBus(L *lua.LState) int {
-	deviceId := L.ToString(1) // 设备名称
-	points := L.ToTable(2)    // 点位值
-
-	deviceData := plugin.DeviceData{
-		ID: deviceId,
-	}
-	var pd []plugin.PointData
-	// 循环点位数据
-	points.ForEach(func(key lua.LValue, value lua.LValue) {
-		pd = append(pd, plugin.PointData{
-			PointName: key.String(),
-			Value:     value.String(),
-		})
-	})
-	deviceData.Values = pd
-
-	PointCacheFilter(&deviceData)
-
-	// 发送数据
-	if len(deviceData.Values) > 0 {
-		for _, export := range export.Exports {
-			if export.IsReady() {
-				export.ExportTo(deviceData)
-			}
-		}
-	}
-	//WriteToMessageBus(deviceData)
-
-	return 0
-}
+//func (lm *LuaModule) WriteToMsgBus(L *lua.LState) int {
+//	deviceId := L.ToString(1) // 设备名称
+//	points := L.ToTable(2)    // 点位值
+//
+//	deviceData := plugin.DeviceData{
+//		ID: deviceId,
+//	}
+//	var pd []plugin.PointData
+//	// 循环点位数据
+//	points.ForEach(func(key lua.LValue, value lua.LValue) {
+//		pd = append(pd, plugin.PointData{
+//			PointName: key.String(),
+//			Value:     value.String(),
+//		})
+//	})
+//	deviceData.Values = pd
+//
+//	PointCacheFilter(&deviceData)
+//
+//	// 发送数据
+//	if len(deviceData.Values) > 0 {
+//		for _, export := range export.Exports {
+//			if export.IsReady() {
+//				export.ExportTo(deviceData)
+//			}
+//		}
+//	}
+//	//WriteToMessageBus(deviceData)
+//
+//	return 0
+//}
--- driverbox/plugin/callback/callback.go
+++ driverbox/plugin/callback/callback.go
@@ -1,10 +1,16 @@
 package callback
 
 import (
+	"fmt"
+	"github.com/ibuilding-x/driver-box/driverbox/config"
+	"github.com/ibuilding-x/driver-box/driverbox/event"
 	"github.com/ibuilding-x/driver-box/driverbox/helper"
 	"github.com/ibuilding-x/driver-box/driverbox/plugin"
 	"github.com/ibuilding-x/driver-box/internal/export"
+	"github.com/ibuilding-x/driver-box/internal/library"
 	"go.uber.org/zap"
+	"strconv"
+	"strings"
 )
 
 // 插件收到通讯消息后，触发该回调方法进行消息解码和设备数据解析
@@ -23,7 +29,7 @@ func ExportTo(deviceData []plugin.DeviceData) {
 	helper.Logger.Debug("export data", zap.Any("data", deviceData))
 	// 写入消息总线
 	for _, data := range deviceData {
-		helper.PointCacheFilter(&data)
+		pointCacheFilter(&data)
 		//触发事件通知
 		if len(data.Events) > 0 {
 			for _, event := range data.Events {
@@ -38,5 +44,129 @@ func ExportTo(deviceData []plugin.DeviceData) {
 				export.ExportTo(data)
 			}
 		}
+	}
+}
+func pointCacheFilter(deviceData *plugin.DeviceData) {
+	// 设备层驱动，对点位进行预处理
+	err := pointValueProcess(deviceData)
+	if err != nil {
+		helper.Logger.Error("device driver process error", zap.Error(err))
+		return
+	}
+	//获取完成点位加工后的真实 deviceData
+	export.TriggerEvents(event.EventCodeWillExportTo, deviceData.ID, plugin.DeviceData{
+		ID:         deviceData.ID,
+		Values:     deviceData.Values,
+		Events:     deviceData.Events,
+		ExportType: deviceData.ExportType,
+	})
+
+	// 定义一个空的整型数组
+	var points []plugin.PointData
+	for _, point := range deviceData.Values {
+		// 获取点位信息
+		p, ok := helper.CoreCache.GetPointByDevice(deviceData.ID, point.PointName)
+		if !ok {
+			//todo 临时屏蔽vrf异常日志输出
+			if !strings.HasPrefix(deviceData.ID, "vrf/") {
+				helper.Logger.Error("unknown point", zap.Any("deviceId", deviceData.ID), zap.Any("pointName", point.PointName))
+			}
+			continue
+		}
+
+		// 缓存比较
+		shadowValue, _ := helper.DeviceShadow.GetDevicePoint(deviceData.ID, point.PointName)
+
+		// 如果是周期上报模式，且缓存中有值，停止触发
+		if p.ReportMode == config.ReportMode_Change && shadowValue == point.Value {
+			helper.Logger.Debug("point report mode is change, stop to trigger ExportTo", zap.String("pointName", p.Name))
+		} else {
+			// 点位值类型名称转换
+			points = append(points, point)
+		}
+
+		// 缓存
+		if err := helper.DeviceShadow.SetDevicePoint(deviceData.ID, point.PointName, point.Value); err != nil {
+			helper.Logger.Error("shadow store point value error", zap.Error(err), zap.Any("deviceId", deviceData.ID))
+		}
+	}
+	deviceData.Values = points
+	deviceData.ExportType = plugin.RealTimeExport
+}
+
+func pointValueProcess(deviceData *plugin.DeviceData) error {
+	device, ok := helper.CoreCache.GetDevice(deviceData.ID)
+	if !ok {
+		helper.Logger.Error("unknown device", zap.Any("deviceId", deviceData.ID))
+		return fmt.Errorf("unknown device")
+	}
+	driverEnable := len(device.DriverKey) > 0
+
+	//通过设备层驱动对点位值进行加工
+	if driverEnable {
+		result := library.Driver().DeviceDecode(device.DriverKey, library.DeviceDecodeRequest{DeviceId: deviceData.ID, Points: deviceData.Values})
+		if result.Error != nil {
+			helper.Logger.Error("library.DeviceDecode error", zap.Error(result.Error), zap.Any("deviceData", deviceData))
+			return result.Error
+		} else {
+			deviceData.Values = result.Points
+		}
+	}
+	for i, p := range deviceData.Values {
+		point, ok := helper.CoreCache.GetPointByDevice(deviceData.ID, p.PointName)
+		if !ok {
+			//todo 临时屏蔽vrf异常日志输出
+			if !strings.HasPrefix(deviceData.ID, "vrf/") {
+				helper.Logger.Error("unknown point", zap.Any("deviceId", deviceData.ID), zap.Any("pointName", p.PointName))
+			}
+			continue
+		}
+		//点位值类型还原
+		value, err := helper.ConvPointType(p.Value, point.ValueType)
+		if err != nil {
+			helper.Logger.Error("convert point value error", zap.Error(err), zap.Any("deviceId", deviceData.ID),
+				zap.String("pointName", p.PointName), zap.Any("value", p.Value))
+			continue
+		}
+
+		//精度换算
+		if !driverEnable && point.Scale != 0 {
+			value, err = multiplyWithFloat64(value, point.Scale)
+			if err != nil {
+				helper.Logger.Error("multiplyWithFloat64 error", zap.Error(err), zap.Any("deviceId", deviceData.ID))
+				continue
+			}
+		}
+
+		//浮点类型,且readValue包含小数时作小数保留位数加工
+		if point.ValueType == config.ValueType_Float && value != 0 {
+			val := fmt.Sprintf("%.*f", point.Decimals, value)
+			value, _ = strconv.ParseFloat(val, 64)
+		}
+		deviceData.Values[i].Value = value
+	}
+	return nil
+}
+
+func multiplyWithFloat64(value interface{}, scale float64) (float64, error) {
+	switch v := value.(type) {
+	case float64:
+		return v * scale, nil
+	case int16:
+		return float64(v) * scale, nil
+	case uint16:
+		return float64(v) * scale, nil
+	case uint32:
+		return float64(v) * scale, nil
+	case int32:
+		return float64(v) * scale, nil
+	case int64:
+		return float64(v) * scale, nil
+	case uint64:
+		return float64(v) * scale, nil
+	case float32:
+		return float64(v) * scale, nil
+	default:
+		return 0, fmt.Errorf("cannot multiply %T with float64", value)
 	}
 }
