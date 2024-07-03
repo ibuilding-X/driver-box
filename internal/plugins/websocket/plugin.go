541310e24f5f37b64aa40280d02ea22bc61f04f5
zhengjw22 <zhengjunweimail@163.com>
2024-07-03 16:52:58 +0800

    增加 websocket 协议插件

--- internal/plugins/websocket/plugin.go
+++ internal/plugins/websocket/plugin.go
@@ -1,7 +1,7 @@
 package websocket
 
 import (
-	"github.com/ibuilding-x/driver-box/driverbox/common"
+	"errors"
 	"github.com/ibuilding-x/driver-box/driverbox/config"
 	"github.com/ibuilding-x/driver-box/driverbox/helper"
 	"github.com/ibuilding-x/driver-box/driverbox/plugin"
@@ -15,12 +15,13 @@ const ProtocolName = "websocket"
 type Plugin struct {
 	config config.Config // 核心配置
 
-	connPool []*connector // 连接器
-	ls       *lua.LState  // lua 虚拟机
+	connPool map[string]*connector // 连接器
+	ls       *lua.LState           // lua 虚拟机
 }
 
 func (p *Plugin) Initialize(logger *zap.Logger, c config.Config, ls *lua.LState) (err error) {
 	p.config = c
+	p.connPool = make(map[string]*connector)
 	p.ls = ls
 
 	// 初始化连接池
@@ -32,8 +33,17 @@ func (p *Plugin) Initialize(logger *zap.Logger, c config.Config, ls *lua.LState)
 }
 
 // Connector 此协议不支持获取连接器
-func (p *Plugin) Connector(deviceSn, pointName string) (connector plugin.Connector, err error) {
-	return nil, common.NotSupportGetConnector
+func (p *Plugin) Connector(deviceId, pointName string) (connector plugin.Connector, err error) {
+	// 获取连接key
+	device, ok := helper.CoreCache.GetDeviceByDeviceAndPoint(deviceId, pointName)
+	if !ok {
+		return nil, errors.New("not found device connection key")
+	}
+	c, ok := p.connPool[device.ConnectionKey]
+	if !ok {
+		return nil, errors.New("not found connection key, key is " + device.ConnectionKey)
+	}
+	return c, nil
 }
 
 func (p *Plugin) Destroy() error {
@@ -63,7 +73,7 @@ func (p *Plugin) initConnPool() (err error) {
 			connMappingDevice: &sync.Map{},
 		}
 		conn.startServer()
-		p.connPool = append(p.connPool, conn)
+		p.connPool[key] = conn
 	}
 	return
 }
