e4a1ef7045109e6d4e9037652f104d97f058c9ff
zhengjw22 <zhengjunweimail@163.com>
2025-07-29 10:49:44 +0800

    feat(crontab): 增加对 crontab 格式定时任务的支持
    - 引入 robfig/cron/v3 包实现 crontab 功能
    - 重构 crontab 结构体，添加 cron.Cron 实例- 修改 AddFunc 方法，支持 crontab 格式的任务调度- 优化 Future 结构体，增加 cronId 字段用于标识 crontab 任务
    - 更新 Disable 方法，支持停止 crontab任务

--- driverbox/helper/crontab/crontab.go
+++ driverbox/helper/crontab/crontab.go
@@ -1,6 +1,7 @@
 package crontab
 
 import (
+	"github.com/robfig/cron/v3"
 	"sync"
 	"time"
 )
@@ -16,7 +17,10 @@ type Crontab interface {
 
 func Instance() Crontab {
 	once.Do(func() {
-		instance = &crontab{}
+		instance = &crontab{
+			c: cron.New(cron.WithSeconds()),
+		}
+		instance.c.Start()
 	})
 	return instance
 }
@@ -26,11 +30,13 @@ type Future struct {
 	function func()
 	//定时器
 	ticker *time.Ticker
+	cronId cron.EntryID
 	//是否启用
 	enable bool
 }
 type crontab struct {
 	futures []*Future
+	c       *cron.Cron
 }
 
 func (c *crontab) Clear() {
@@ -44,16 +50,27 @@ func (c *crontab) Clear() {
 
 func (c *crontab) AddFunc(s string, f func()) (*Future, error) {
 	d, err := time.ParseDuration(s)
+	if err == nil {
+		function := &Future{
+			function: f,
+			ticker:   time.NewTicker(d),
+			enable:   true,
+		}
+		c.futures = append(c.futures, function)
+		go function.run()
+		return function, nil
+	}
+	//尝试按照crontab格式添加
+	cronId, err := c.c.AddFunc(s, f)
 	if err != nil {
 		return &Future{}, err
 	}
 	function := &Future{
 		function: f,
-		ticker:   time.NewTicker(d),
+		cronId:   cronId,
 		enable:   true,
 	}
 	c.futures = append(c.futures, function)
-	go function.run()
 	return function, nil
 }
 
@@ -68,5 +85,10 @@ func (f *Future) run() {
 }
 func (f *Future) Disable() {
 	f.enable = false
-	f.ticker.Stop()
+	if f.ticker != nil {
+		f.ticker.Stop()
+	}
+	if f.cronId != 0 {
+		instance.c.Remove(f.cronId)
+	}
 }
